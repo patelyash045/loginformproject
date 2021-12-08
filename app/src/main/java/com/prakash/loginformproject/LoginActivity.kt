f3fe268605a931239c43b7e47a8457f30f2c6a7a
yash.patel <patelyash045@gmail.com>
2021-12-08 19:02:40 +0530

    Add Google button

--- app/src/main/java/com/prakash/loginformproject/LoginActivity.kt
+++ app/src/main/java/com/prakash/loginformproject/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.prakash.loginformproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.prakash.loginformproject.activity.SignupActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var txtsignup : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        txtsignup = findViewById(R.id.txtSignup)
+
+        txtsignup.setOnClickListener {
+            val intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
